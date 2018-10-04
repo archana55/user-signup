5324cd44906727aaeacbf6e14d5d657d743c1e89
Archana1 <archana1@doe.org>
2018-10-04 00:05:00 -0500

    added templates

--- main.py
+++ main.py
@@ -1,70 +1,25 @@
 from flask import Flask, request,redirect
 import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-<!doctype html>
-<html>
-    <body>
-    <style>
-        .error {{ color: red; }}
-    </style>
+#form = """
 
-        <form  method="post">
-        <table>
-        <tr>
-           <td>
-              <label for="user-name">Username:</label>
-           </td>
-           <td>
-              <input id="user-name" type="text"  name="user_name_input" value ="{name}" />
-           </td>
-          <td> <span class="error">{username_error}</span></td>
-        </tr>
-        <tr>
-           <td>
-              <label for="pass-word">Password:</label>
-           </td>
-           <td>
-              <input id="pass-word" type="password" name="pass_word_input" />
-           </td>
-           <td> <span class="error">{password_error}</span></td>
-        </tr>
-        <tr>
-           <td>
-              <label for="verify-password">Verify Password:</label>
-           </td>
-           <td>
-              <input id="verify-password" type="password" name="verify_pass_word_input" />
-           </td>
-           <td> <span class="error">{verify_error}</span></td>
-         </tr>
-         <tr>
-           <td>
-              <label for="email">Email(optional):</label>
-           </td>
-           <td>
-               <input id="email" type="text" name="email_input" value ="{emailname}"/>
-           </td>
-           <td> <span class="error">{email_error}</span></td>
-        </tr>
-        <tr>
-           <td>
-              <input type="submit" value ="Submit" />
-           </td>
-        </tr>
-        </table>
-        </form>
-    </body>
-</html>
-"""
+#"""
 
 @app.route("/")
 def index():
-    return form.format(username_error='',password_error ='',
-    verify_error = '',email_error = '',name ='',emailname='')
+    template = jinja_env.get_template('index.html')
+    return template.render()
+# return form.format(username_error='',password_error ='',
+ #   verify_error = '',email_error = '',name ='',emailname='')
 
 
 @app.route("/", methods=['POST'])
@@ -84,7 +39,8 @@ def validate():
     if not error_username and not error_password and not error_verifypwd and not error_email :
         return redirect('/hello?name={0}'.format(username))
     else:
-        return form.format(username_error = error_username,
+        template = jinja_env.get_template('index.html')
+        return template.render(username_error = error_username,
         password_error =error_password,
         verify_error = error_verifypwd , email_error = error_email ,
         name = username , emailname = email_input)
